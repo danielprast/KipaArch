a357400445dced4e1a4c8ea4f5b7dc27b84ce486
Daniel Prastiwa <daniel.prastiwa@akupeduli.org>
2022-02-21 14:39:33 +0700

    clean up code

--- Tests/KipaArchTests/KipaDIContainerTests.swift
+++ Tests/KipaArchTests/KipaDIContainerTests.swift
@@ -1,11 +1,11 @@
 import XCTest
-@testable import KipaDIContainer
+@testable import KipaArch
 
 final class KipaDIContainerTests: XCTestCase {
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(KipaDIContainer().text, "Hello, World!")
+        //XCTAssertEqual(KipaDIContainer().text, "Hello, World!")
     }
 }
