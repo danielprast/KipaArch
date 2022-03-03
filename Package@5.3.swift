428b1590aa9403073fb8119b488169337a5eab90
Daniel Prastiwa <danielprastiwa@gmail.com>
2022-03-03 14:10:12 +0700

    Update package swift manifest file

--- Package@5.3.swift
+++ Package@5.3.swift
@@ -1,5 +1,10 @@
-// swift-tools-version:5.5
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-tools-version:5.3
+//
+//  Package@5.3.swift
+//  KipaArch
+//
+//  Created by Daniel Prastiwa on 03/03/22.
+//
 
 import PackageDescription
 
@@ -13,7 +18,8 @@ let package = Package(
     // Products define the executables and libraries a package produces, and make them visible to other packages.
     .library(
       name: "KipaArch",
-      targets: ["KipaArch"]),
+      targets: ["KipaArch"]
+    ),
   ],
   dependencies: [
     // Dependencies declare other packages that this package depends on.
@@ -24,9 +30,11 @@ let package = Package(
     // Targets can depend on other targets in this package, and on products in packages this package depends on.
     .target(
       name: "KipaArch",
-      dependencies: []),
+      dependencies: []
+    ),
     .testTarget(
       name: "KipaArchTests",
-      dependencies: ["KipaArch"]),
+      dependencies: ["KipaArch"]
+    ),
   ]
 )
